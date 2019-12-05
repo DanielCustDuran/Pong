b645eb6f17c0ed6f5e2b3fdf9352f54c670634ef
Daniel Isidro Custodio Duran <danielisidrocustodioduran@gmail.com>
2019-12-05 14:35:15 -0600

    Movement of ball, created main scene.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Transform paddle;
+    bool gameStarted = false;
+    Rigidbody2D rgBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgBall = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            float posDiff = paddle.position.x - transform.position.x;
+            transform.position = new Vector3(paddle.position.x - posDiff, paddle.position.y, paddle.position.z);
+            if (Input.GetMouseButtonDown(0))
+            {
+                gameStarted = true;
+                rgBall.velocity = new Vector2(5, 5);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(cameraPos.y, -3.8f, 3.8f), transform.position.z);
+    }
+}
