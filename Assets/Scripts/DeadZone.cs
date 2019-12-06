15d0c89b47b6200406635f7bdf6e8b9291f2958d
Daniel Isidro Custodio Duran <danielisidrocustodioduran@gmail.com>
2019-12-06 06:08:51 -0600

    Created Deadzone for scoring of player or enemy.

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour
+{
+    public Text scorePlayerText;
+    public Text scoreEnemyText;
+
+    int scorePlayerQuantity;
+    int scoreEnemyQuantity;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.CompareTag("Left"))
+        {
+            scoreEnemyQuantity++;
+            UpdateScoreLabel(scoreEnemyText, scoreEnemyQuantity);
+        }else if (gameObject.CompareTag("Right"))
+        {
+            scorePlayerQuantity++;
+            UpdateScoreLabel(scorePlayerText, scoreEnemyQuantity);
+        }
+    }
+
+    void UpdateScoreLabel(Text label, int score)
+    {
+        label.text = score.ToString();
+    }
+}
